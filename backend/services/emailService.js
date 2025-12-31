fad18c967066283d5a05125ac9b56837648daf3f
nikkeykumar <nikkeykumar161@gmail.com>
2025-12-31 13:34:24 +0530

    fix 1

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -3,13 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
-console.log(process.env.EMAIL_USER, process.env.EMAIL_PASS),
+
   transporter.verify((error, success) => {
     if (error) {
       console.error("Gmail service connection failed");
